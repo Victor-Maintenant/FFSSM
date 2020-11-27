e6f9d1c0c3b3435996e0d74637f59c903a8cdd03
Victor <victor.maintenant@etud.univ-jfc.fr>
2020-11-27 14:23:06 +0100

    Fin Classe Moniteur

--- src/main/java/FFSSM/Moniteur.java
+++ src/main/java/FFSSM/Moniteur.java
@@ -4,12 +4,16 @@
 package FFSSM;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class Moniteur extends Plongeur {
 
     public int numeroDiplome;
+    private final Map<Club, Embauche> emplois = new HashMap<>();
 
     public Moniteur(String numeroINSEE, String nom, String prenom, String adresse, 
     		String telephone, LocalDate naissance, int numeroDiplome, int niveau, GroupeSanguin groupe, int numDiplome ) {
@@ -22,9 +26,14 @@ public class Moniteur extends Plongeur {
      * ce moniteur n'a pas d'employeur.
      * @return l'employeur actuel de ce moniteur sous la forme d'un Optional
      */
-    public Optional<Club> employeurActuel() {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    public Optional<Club> employeurActuel() throws Exception {
+    	Club c = new Club(null, null, null);
+    	for (Club club : this.emplois.keySet()) {
+    		c = club;
+    	}
+    	if (this.emplois.isEmpty()) throw new Exception("Aucun emploi");
+    	if (this.emplois.get(c).estTerminee()) throw new Exception("Dernier emploi déjà terminé");
+    	return Optional.ofNullable(this.emplois.get(c).getEmployeur());
     }
     
     /**
@@ -33,13 +42,21 @@ public class Moniteur extends Plongeur {
      * @param debutNouvelle la date de début de l'embauche
      */
     public void nouvelleEmbauche(Club employeur, LocalDate debutNouvelle) {   
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");	    
+    	Embauche e = new Embauche(debutNouvelle, this, employeur);
+    	this.emplois.put(employeur, e);
     }
 
+    
+    /**
+     * retourne la liste des embauches du moniteur
+     * @return la liste des embauches
+     */
     public List<Embauche> emplois() {
-         // TODO: Implémenter cette méthode
-        throw new UnsupportedOperationException("Pas encore implémenté");
+    	List<Embauche> e = new LinkedList<>();
+    	this.emplois.forEach((club, embauche) ->{
+    		e.add(embauche);
+    	});
+    	return e;
     }
 
 }
